ac158568cafde7d331ed41bfdfdc7f5c10de9cdd
6mbp5c8hyq-collab <6mbp5c8hyq@privaterelay.appleid.com>
2026-09-15 00:47:52 +0300

    Expose Chemistry Suite from native Command Center

--- AURORA/CommandCenterView.swift
+++ AURORA/CommandCenterView.swift
@@ -3,5 +3,14 @@ import SwiftUI
 struct CommandCenterView: View {
     var body: some View {
         GovernedCommandCenterView()
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink {
+                        ChemistryWorkbenchView()
+                    } label: {
+                        Label("Chemistry Suite", systemImage: "atom")
+                    }
+                }
+            }
     }
 }
