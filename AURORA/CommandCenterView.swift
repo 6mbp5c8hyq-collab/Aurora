4bc1b940caf2427f03e1eec155fe7e3a997f6e15
6mbp5c8hyq-collab <6mbp5c8hyq@privaterelay.appleid.com>
2026-09-17 20:07:31 +0300

    Expose Scientific Project Basis V3 from Command Center

--- AURORA/CommandCenterView.swift
+++ AURORA/CommandCenterView.swift
@@ -1,7 +1,30 @@
 import SwiftUI
 
 struct CommandCenterView: View {
+    @State private var showScientificProjectBasisV3 = false
+
     var body: some View {
         GovernedCommandCenterView()
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        showScientificProjectBasisV3 = true
+                    } label: {
+                        Label("Project Basis V3", systemImage: "scope")
+                    }
+                }
+            }
+            .sheet(isPresented: $showScientificProjectBasisV3) {
+                NavigationStack {
+                    ScientificProjectBasisV3View()
+                        .toolbar {
+                            ToolbarItem(placement: .topBarTrailing) {
+                                Button("Done") {
+                                    showScientificProjectBasisV3 = false
+                                }
+                            }
+                        }
+                }
+            }
     }
 }
